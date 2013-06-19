e0c63a242066a28c2d4ae913702132def10b1336
Stephen Mcgregor <tehborris@gmail.com>
2013-06-19 19:06:40 +1000

    multi spawn enabled

--- src/nu/nerd/nerdspawn/NerdSpawn.java
+++ src/nu/nerd/nerdspawn/NerdSpawn.java
@@ -1,8 +1,12 @@
 package nu.nerd.nerdspawn;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,20 +16,44 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class NerdSpawn extends JavaPlugin
 {
 	private final NerdSpawnListener listener = new NerdSpawnListener(this);
+        List<String> spawnList = new ArrayList<String>();
 
 	public Location getSpawnLocation()
 	{
 		// just in case someone edited the file
 		reloadConfig();
+                
+                spawnList.clear();
+                spawnList.addAll(getConfig().getStringList("spawn.location"));
 
-		return new Location(
-				getServer().getWorlds().get(0),
-				getConfig().getDouble("spawn-location.x"),
-				getConfig().getDouble("spawn-location.y"),
-				getConfig().getDouble("spawn-location.z"),
-				(float)getConfig().getDouble("spawn-location.yaw"),
-				(float)getConfig().getDouble("spawn-location.pitch"));
+                if( getConfig().getBoolean("many-spawn") == true) {
+                    Random r = new Random();
+                    return stringToLocation(spawnList.get(r.nextInt(spawnList.size())));
+                } else {
+                    return stringToLocation(spawnList.get(0));
+                }
 	}
+        
+        public Location stringToLocation(String location) {
+            String[] lp = location.split("\\|");
+            World world = getServer().getWorld(lp[0]);
+            return new Location(world, 
+                    Double.valueOf(lp[1]), 
+                    Double.valueOf(lp[2]), 
+                    Double.valueOf(lp[3]), 
+                    Float.valueOf(lp[4]), 
+                    Float.valueOf(lp[5]));
+        }
+        
+        public String locationToString(Location location) {
+            String re = location.getWorld().getName() + "|";
+            re += location.getX() + "|";
+            re += location.getY() + "|";
+            re += location.getZ() + "|";
+            re += location.getYaw() + "|";
+            re += location.getPitch();
+            return re;
+        }
 
 	@Override
 	public void onEnable()
@@ -37,6 +65,9 @@ public class NerdSpawn extends JavaPlugin
 			getConfig().options().copyDefaults(true);
 			saveConfig();
 		}
+                saveConfig();
+                spawnList.clear();
+                spawnList.addAll(getConfig().getStringList("spawn.location"));
 	}
 
 	@Override
@@ -60,27 +91,27 @@ public class NerdSpawn extends JavaPlugin
 		}
 
 		if (command.getName().equalsIgnoreCase("setspawn")) {
-			setSpawn((Player) sender,"spawn-location");
+			setSpawn((Player) sender);
 			return true;
 		}
 
 		return false;
 	}
 
-	private void setSpawn(Player player,String node)
+	private void setSpawn(Player player)
 	{
 		Location loc = player.getLocation();
 		if (!loc.getWorld().equals(getServer().getWorlds().get(0))) {
 			player.sendMessage(ChatColor.GRAY + "Spawn can only be set in main world.");
 			return;
 		}
-
-		getConfig().set(node + ".x", loc.getX());
-		getConfig().set(node + ".y", loc.getY());
-		getConfig().set(node + ".z", loc.getZ());
-		getConfig().set(node + ".yaw", loc.getYaw());
-		getConfig().set(node + ".pitch", loc.getPitch());
-		saveConfig();
+                
+                if( getConfig().getBoolean("many-spawn") != true) {
+                    spawnList.clear();
+                } 
+                spawnList.add(locationToString(loc));
+		getConfig().set("spawn.location", spawnList);
+                saveConfig();
 
 		getServer().getWorlds().get(0).setSpawnLocation(
 				loc.getBlockX(),
