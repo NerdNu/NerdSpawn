5b78cf2b7907819925e07e6273ef74b35b6160ae
totemo <TotemoResearch@gmail.com>
2013-12-20 20:16:28 +1030

    Add an option to respawn at spawn if they die too close to their bed.

--- src/nu/nerd/nerdspawn/NerdSpawnListener.java
+++ src/nu/nerd/nerdspawn/NerdSpawnListener.java
@@ -1,26 +1,33 @@
 package nu.nerd.nerdspawn;
 
+import java.util.HashMap;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.World.Environment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
-
-public class NerdSpawnListener implements Listener
-{
+public class NerdSpawnListener implements Listener {
     private final NerdSpawn plugin;
 
-    public NerdSpawnListener(NerdSpawn instance)
-    {
+    /**
+     * Map from player name to most recent death location. Does not survive
+     * restarts, sorry.
+     */
+    private HashMap<String, Location> deathLocations = new HashMap<String, Location>();
+
+    public NerdSpawnListener(NerdSpawn instance) {
         plugin = instance;
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         if (!event.getPlayer().hasPlayedBefore()) {
             event.getPlayer().teleport(plugin.getSpawnLocation());
             if (plugin.getConfig().getBoolean("show-join-message")) {
@@ -33,17 +40,41 @@ public class NerdSpawnListener implements Listener
             }
         }
     }
-        
-    @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent event)
-    {
-        if (!event.isBedSpawn() || !plugin.getConfig().getBoolean("allow-bed-spawn"))
-            event.setRespawnLocation(plugin.getSpawnLocation());
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        deathLocations.put(event.getEntity().getName(), event.getEntity().getLocation());
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        if (event.isBedSpawn()) {
+            boolean goToSpawn = false;
+            if (plugin.getConfig().getBoolean("allow-bed-spawn")) {
+                if (plugin.getConfig().getBoolean("check-bed-radius")) {
+                    Player player = event.getPlayer();
+                    Location deathLocation = deathLocations.get(player.getName());
+                    if (deathLocation != null) {
+                        double bedRadius = plugin.getConfig().getDouble("bed-radius", 15);
+                        if (player.getBedSpawnLocation().distanceSquared(deathLocation) < bedRadius * bedRadius) {
+                            player.sendMessage(ChatColor.GOLD + "You died too close to your bed. Sending you back to spawn.");
+                            goToSpawn = true;
+                        }
+                    }
+                }
+            } else {
+                goToSpawn = true;
+            }
+
+            if (goToSpawn) {
+                event.setRespawnLocation(plugin.getSpawnLocation());
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerPortal(PlayerPortalEvent event) {
-        if(event.getFrom().getWorld().getEnvironment() == Environment.THE_END && !plugin.getConfig().getBoolean("allow-bed-spawn")) {
+        if (event.getFrom().getWorld().getEnvironment() == Environment.THE_END && !plugin.getConfig().getBoolean("allow-bed-spawn")) {
             event.setTo(plugin.getSpawnLocation());
         }
     }
